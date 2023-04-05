fb351556c1e22ae926fa6fb39344bdbe97abed01
DavidPaulinus <davipm@hotmail.com>
2023-04-05 11:11:52 -0300

    Invocando métodos com parâmetros

--- src/br/com/alura/estudo/controle/Controle.java
+++ src/br/com/alura/estudo/controle/Controle.java
@@ -16,6 +16,21 @@ public class Controle {
 
 	private void metodoControle1() {}
 	
+	public void metodoControle2(String str) {
+		System.out.println("Executando Controle.metodoControle2(String str)");
+		System.out.println("Parâmetro: " + str);
+	}
+	public void metodoControle2(String str, String str2) {
+		System.out.println("Executando Controle.metodoControle2(String str, String str2)");
+		System.out.println("Parâmetro: " + str);
+		System.out.println("Parâmetro: " + str2);
+	}
+	public void metodoControle2(String str, Integer in) {
+		System.out.println("Executando Controle.metodoControle2(String str, Integer in)");
+		System.out.println("Parâmetro: " + str);
+		System.out.println("Parâmetro: " + in);
+	}
+	
 	public List<String> getList() {
 		return lista;
 	}
--- src/br/com/alura/estudo/reflexao/TesteInvocaMetodo.java
+++ src/br/com/alura/estudo/reflexao/TesteInvocaMetodo.java
@@ -5,27 +5,34 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TesteInvocaMetodo {
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
-		Class<?> subControle = Class.forName("br.com.alura.estudo.controle.SubControle") ;
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
+			IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+//		Class<?> subControle = Class.forName("br.com.alura.estudo.controle.SubControle") ;
+//		
+//		Constructor<?> construtor = subControle.getDeclaredConstructor();
+//		construtor.setAccessible(true);
+//		
+//		Object obj = construtor.newInstance();
+//		
+//		for(Method m: subControle.getMethods()) {
+//			System.out.println(m);
+//		}
+//		System.out.println("\n==========================\n");
+//		for(Method m: subControle.getDeclaredMethods()) {
+//			System.out.println(m);
+//		}
+//		
+//		Method method = subControle.getDeclaredMethod("metodo2");
+//		method.setAccessible(true);
+//		
+//		Object retorno = method.invoke(obj);
+//		System.out.println(retorno);
+		Class<?> controleClass = Class.forName("br.com.alura.estudo.controle.Controle");
+		Object controle = controleClass.getDeclaredConstructor().newInstance();
 		
-		Constructor<?> construtor = subControle.getDeclaredConstructor();
-		construtor.setAccessible(true);
+		Method m = controleClass.getDeclaredMethod("metodoControle2", String.class, Integer.class);
+		Object retorno = m.invoke(controle, "AAA", 1);
 		
-		Object obj = construtor.newInstance();
-		
-		for(Method m: subControle.getMethods()) {
-			System.out.println(m);
-		}
-		System.out.println("\n==========================\n");
-		for(Method m: subControle.getDeclaredMethods()) {
-			System.out.println(m);
-		}
-		
-		Method method = subControle.getDeclaredMethod("metodo2");
-		method.setAccessible(true);
-		
-		Object retorno = method.invoke(obj);
 		System.out.println(retorno);
-		
 	}
 }
